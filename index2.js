9752665d2f54034d3a1a0cf7fb9ca74cab865ec1
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-03-01 13:49:26 -0700

    function inside ajax call working

--- index2.js
+++ index2.js
@@ -1,42 +1,61 @@
 $(document).ready(function() {
-// event listener for button
-$('#search-color-selector-btn').click(function() {
-  console.log('you clicked');
-  //user input value minus '#'
-let userSearch = $('#color-selector').val().slice(1);
-console.log('user input', userSearch);
-//set up variable to contain data
-let selectColorData= []
-$.ajax({
-      url: `http://www.colourlovers.com/api/palettes`,
-      dataType:'jsonp',
-      data: {
-            format: 'json',
-            numResults: 50,
-            hex: userSearch,
-        },
-      jsonp: 'jsonCallback',
-      success: function(data){
-        console.log(data);
+    //append div function
+    function createPaletteDiv(data) {
+        let paletteDiv = $('<div>').addClass("palette-div")
+        paletteDiv.appendTo('#color-selector-box')
 
-      }
-  });
+    }
 
+    // event listener for button
+    $('#search-color-selector-btn').click(function() {
+        console.log('you clicked');
+        //user input value minus '#'
+        let userSearch = $('#color-selector').val().slice(1);
+        console.log('user input', userSearch);
+        //set up variable to contain data
+        let selectColorData = [];
+        $.ajax({
+            url: `http://www.colourlovers.com/api/palettes`,
+            dataType: 'jsonp',
+            data: {
+                format: 'json',
+                numResults: 50,
+                hex: userSearch,
+            },
+            jsonp: 'jsonCallback',
+            success: function(data) {
+                console.log(data[0]);
+                //append container for each palette
+                for (var i = 0; i < data.length; i++) {
+                    createPaletteDiv(data[i])
+                }
 
-// event listener close
-})
-$('#search-color-btn').click(function(){
-let userSearchTwo= $('#search-color-text').val()
-console.log(userSearchTwo);
-
-
-
-  //event listener2 (#search-color-btn) close
-})
+                // console.log("div children:", $('#color-selector-box').children());
 
 
 
 
+            } //success block close
 
-  // document ready close
+        });
+
+
+    }) // event listener close
+
+
+    //event listener for search by color name
+    $('#search-color-btn').click(function() {
+        let userSearchTwo = $('#search-color-text').val()
+        console.log('user search 2:', userSearchTwo);
+
+
+
+        //event listener2 (#search-color-btn) close
+    })
+
+
+
+
+
+    // document ready close
 });
