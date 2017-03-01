822a869800a23d6e748bc481ac556e03dd60baab
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-02-28 21:29:32 -0700

    working ajax call getting object back

--- index2.js
+++ index2.js
@@ -1,39 +1,79 @@
 $(document).ready(function() {
-console.log();
 // event listener for button
-$('#search-color').click(function() {
+$('#search-color-selector-btn').click(function() {
   console.log('you clicked');
   //user input value
 let userSearch = $('#color-selector').val().slice(1);
 console.log(userSearch);
+
 $.ajax({
-method: 'GET',
+      url: 'http://www.colourlovers.com/api/palettes/random',
+      dataType:'jsonp',
+      data: {
+          format: 'json'
+      },
+      jsonp: 'jsonCallback',
+      success: function(data){
+        console.log(data);
+        
+      }
+  });
 
-url: `http://www.colourlovers.com/api/palettes/OR/${userSearch}/?format=json`
-,
-
-dataType: 'jsonp',
-success: function(data){
-// what to do if data is retrieved successfully
-console.log(data);
-let collection= $('.collection-ul')
-console.log(collection);
-
-
-
-  //data close
-}
-
-
-
-
-  //ajax close
-});
+// var ajaxCall=$.ajax({
+// method: 'GET',
+// url: 'http://www.colourlovers.com/api/palettes/random/?format=jsonp',
+// dataType: 'jsonp',
+// jsonpCallback: function callback(data) {
+//   console.log('data', data);
+// }
+// })
+//
+// ajaxCall.done(function(data){
+// console.log('data');
+//
+// })
+//
+// ajaxCall.fail(function(error){
+// console.log('error', error);
+//
+// })
+// $.ajax({
+// method: 'GET',
+//
+// // url: `http://www.colourlovers.com/api/palettes/OR/${userSearch}/?format=json`,
+// //I took out ?format=json from the end of your link
+// url: 'http://www.colourlovers.com/api/palettes/random/?format=json',
+// dataType: 'jsonp',
+// success: function(data){
+// // what to do if data is retrieved successfully
+// console.log('data');
+//
+//
+//
+//   //data close
+// }
+//
+//
+//
+//
+//   //ajax close
+// });
 
 
 
 // event listener close
 })
+$('#search-color-btn').click(function(){
+let userSearchTwo= $('#search-color-text').val()
+console.log(userSearchTwo);
+
+
+
+  //event listener2 (#search-color-btn) close
+})
+
+
+
 
 
   // document ready close
