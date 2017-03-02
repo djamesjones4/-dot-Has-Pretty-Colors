f8c05a32e3bd9817694e18723411e24a363541e2
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-03-02 14:46:42 -0700

    added rainbow colors div to index2

--- save.js
+++ save.js
@@ -14,8 +14,33 @@ $('nav').css('background-color', `${colorToUse}`)
 
 })
 
+$('.palettecontainer2').click(function(){
+  console.log('you clicked');
 
+if($(event.target.id) !== "pal-cont-1"){
+  console.log('you clicked');
+  console.log(event.target);
+var colorToUse = $(event.target).css('background-color')
+}
+console.log("target color:", colorToUse);
+console.log($('nav')[0]);
+$('body').css('background-color', `${colorToUse}`)
 
+})
+
+$('.palettecontainer3').click(function(){
+  console.log('you clicked');
+
+if($(event.target.id) !== "pal-cont-1"){
+  console.log('you clicked');
+  console.log(event.target);
+var colorToUse = $(event.target).css('background-color')
+}
+console.log("target color:", colorToUse);
+console.log($('nav')[0]);
+$('.text-color').css('color', `${colorToUse}`)
+
+})
 
 
 
