1781845f77c1a1095e2c71ed9bf9ff2206486acc
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-03-03 11:31:52 -0700

    click and apply working

--- save.js
+++ save.js
@@ -1,86 +1,135 @@
-$(document).ready(function(){
-  var createPalContainer = function createPaletteDiv(data, palCount, divContainer) {
-      let paletteDiv = $('<div>').addClass("palette-div").attr('id', `pal${palCount}`)
-      paletteDiv.css('width', '250px')
-      paletteDiv.css('height', '50px')
-      paletteDiv.css('margin', '10px')
-      // paletteDiv.attr('draggable', true)
-      // paletteDiv.attr('ondragstart', "event.dataTransfer.setData('text/plain',null)")
-      paletteDiv.appendTo(divContainer)
-      if (data !== undefined) {
+$(document).ready(function() {
+    var createPalContainer = function createPaletteDiv(data, palCount, divContainer) {
+        let paletteDiv = $('<div>').addClass("palette-div").attr('id', `pal${palCount}`)
 
-          for (let i = 0; i < data.length; i++) {
+        paletteDiv.css('width', '250px')
+        paletteDiv.css('height', '50px')
+        paletteDiv.css('margin', '10px')
+        // paletteDiv.attr('draggable', true)
+        // paletteDiv.attr('ondragstart', "event.dataTransfer.setData('text/plain',null)")
 
-              let colorDiv = $('<div>').addClass('color-div' [i]).appendTo(paletteDiv)
-              let rgbValue = data[i]
-              console.log("Creating color", rgbValue , "for palette", palCount);
+        if (data !== undefined) {
 
-              console.log(rgbValue);
-              // give each colorDiv a unique classname (index[c,o,l,o,r] + palette title + hexValue)
-              // colorDiv.addClass(`${data.title} ` + `${hexValue}`)
-              // add background color of hexValue to colorDiv
-              colorDiv.css('background-color', 'rgbValue');
-              colorDiv.css('width', '50px');
-              colorDiv.css('height', '50px');
-              colorDiv.css('display', 'inline-block')
-              console.log(paletteDiv);
-          }
-      }
-  } // end create palette fu
-let savedPalettes = JSON.parse(localStorage.getItem('palettes'))
-console.log(savedPalettes);
+            for (let i = 0; i < data.length; i++) {
 
-let divContainer = $('#paletteCont')
-//iterate through saved palettes
-for(let i=0; i<savedPalettes.length; i++){
-  console.log("Creating palette", i);
-  createPalContainer(savedPalettes[i], i, divContainer)
+                let colorDiv = $('<div>').addClass('color-div' [i])
+                let rgbValue = data[i]
+                // let rgbaValue = data[i].slice(0,3) + "a" + data[i].slice(3, (data[i].length -1)) + ", 1" + ")"
+                console.log("Creating color", rgbValue, "for palette", palCount);
 
-}
+                console.log(rgbValue);
+                // give each colorDiv a unique classname (index[c,o,l,o,r] + palette title + hexValue)
+                // colorDiv.addClass(`${data.title} ` + `${hexValue}`)
+                // add background color of hexValue to colorDiv
+                colorDiv.css('background-color', rgbValue);
+                colorDiv.css('width', '50px');
+                colorDiv.css('height', '50px');
+                colorDiv.css('display', 'inline-block')
+                colorDiv.appendTo(paletteDiv)
+                console.log(paletteDiv);
+                console.log(rgbValue, colorDiv.css('background-color'));
+            }
+
+        }
+        paletteDiv.appendTo(divContainer)
+
+        paletteDiv.attr('class', 'back-color').appendTo($('.backgroundColor'))
+        paletteDiv.clone().attr('class', 'nav-color').appendTo($('.navColor'))
+        paletteDiv.clone().attr('class', 'color-text').appendTo($('.textColor'))
+
+    } // end create palette fu
+    let savedPalettes = JSON.parse(localStorage.getItem('palettes'))
+    console.log(savedPalettes);
+
+    let divContainer = $('#paletteCont')
+    //iterate through saved palettes
+    for (let i = 0; i < savedPalettes.length; i++) {
+        console.log("Creating palette", i);
+        createPalContainer(savedPalettes[i], i, divContainer)
+        $('.back-color').click(function() {
+            console.log('you clicked');
+
+            if ($(event.target.id) !== "noClick1" && $(event.target.id) !== 'pal0') {
+                console.log('you clicked');
+                console.log(event.target);
+                var colorToUse = $(event.target).css('background-color')
+            }
+            console.log("target color:", colorToUse);
+            console.log($('nav')[0]);
+            $('body').css('background-color', `${colorToUse}`)
+        })
+        $('.nav-color').click(function() {
+            console.log('you clicked');
+
+            if ($(event.target.id) !== "noClick" && $(event.target.id) !== 'pal1') {
+                console.log('you clicked');
+                console.log(event.target);
+                var colorToUse = $(event.target).css('background-color')
+            }
+            console.log("target color:", colorToUse);
+            console.log($('nav')[0]);
+            $('nav').css('background-color', `${colorToUse}`)
+        })
+
+        $('.color-text').click(function() {
+            console.log('you clicked');
+
+            if ($(event.target.id) !== "noClick" && $(event.target.id) !== 'pal2') {
+                console.log('you clicked');
+                console.log(event.target);
+                var colorToUse = $(event.target).css('background-color')
+            }
+            console.log("target color:", colorToUse);
+            console.log($('nav')[0]);
+            $('.text-color').css('color', `${colorToUse}`)
+
+        })
+
+    }
 
 
 
-// $('.palettecontainer').click(function(){
-//   console.log('you clicked');
-//
-// if($(event.target.id) !== "pal-cont-1"){
-//   console.log('you clicked');
-//   console.log(event.target);
-// var colorToUse = $(event.target).css('background-color')
-// }
-// console.log("target color:", colorToUse);
-// console.log($('nav')[0]);
-// $('nav').css('background-color', `${colorToUse}`)
-//
-// })
-//
-// $('.palettecontainer2').click(function(){
-//   console.log('you clicked');
-//
-// if($(event.target.id) !== "pal-cont-1"){
-//   console.log('you clicked');
-//   console.log(event.target);
-// var colorToUse = $(event.target).css('background-color')
-// }
-// console.log("target color:", colorToUse);
-// console.log($('nav')[0]);
-// $('body').css('background-color', `${colorToUse}`)
-//
-// })
-//
-// $('.palettecontainer3').click(function(){
-//   console.log('you clicked');
-//
-// if($(event.target.id) !== "pal-cont-1"){
-//   console.log('you clicked');
-//   console.log(event.target);
-// var colorToUse = $(event.target).css('background-color')
-// }
-// console.log("target color:", colorToUse);
-// console.log($('nav')[0]);
-// $('.text-color').css('color', `${colorToUse}`)
-//
-// })
+    // $('.back-color').click(function() {
+    //     console.log('you clicked');
+    //
+    //     if ($(event.target.id) !== "noClick1" && $(event.target.id) !== 'pal0') {
+    //         console.log('you clicked');
+    //         console.log(event.target);
+    //         var colorToUse = $(event.target).css('background-color')
+    //     }
+    //     console.log("target color:", colorToUse);
+    //     console.log($('nav')[0]);
+    //     $('body').css('background-color', `${colorToUse}`)
+    //
+    // })
+
+    $('.palettecontainer2').click(function() {
+        console.log('you clicked');
+
+        if ($(event.target.id) !== "pal-cont-1") {
+            console.log('you clicked');
+            console.log(event.target);
+            var colorToUse = $(event.target).css('background-color')
+        }
+        console.log("target color:", colorToUse);
+        console.log($('nav')[0]);
+        $('nav').css('background-color', `${colorToUse}`)
+
+    })
+
+    $('color-text').click(function() {
+        console.log('you clicked');
+
+        if ($(event.target.id) !== "pal-cont-1") {
+            console.log('you clicked');
+            console.log(event.target);
+            var colorToUse = $(event.target).css('background-color')
+        }
+        console.log("target color:", colorToUse);
+        console.log($('nav')[0]);
+        $('.text-color').css('color', `${colorToUse}`)
+
+    })
 
 
 
