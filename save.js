91edc4b790b6c7284b6c37e63b6dbeba127caafc
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-03-03 11:51:01 -0700

    ready to present

--- save.js
+++ save.js
@@ -89,47 +89,7 @@ $(document).ready(function() {
 
 
 
-    // $('.back-color').click(function() {
-    //     console.log('you clicked');
-    //
-    //     if ($(event.target.id) !== "noClick1" && $(event.target.id) !== 'pal0') {
-    //         console.log('you clicked');
-    //         console.log(event.target);
-    //         var colorToUse = $(event.target).css('background-color')
-    //     }
-    //     console.log("target color:", colorToUse);
-    //     console.log($('nav')[0]);
-    //     $('body').css('background-color', `${colorToUse}`)
-    //
-    // })
-
-    $('.palettecontainer2').click(function() {
-        console.log('you clicked');
-
-        if ($(event.target.id) !== "pal-cont-1") {
-            console.log('you clicked');
-            console.log(event.target);
-            var colorToUse = $(event.target).css('background-color')
-        }
-        console.log("target color:", colorToUse);
-        console.log($('nav')[0]);
-        $('nav').css('background-color', `${colorToUse}`)
-
-    })
-
-    $('color-text').click(function() {
-        console.log('you clicked');
-
-        if ($(event.target.id) !== "pal-cont-1") {
-            console.log('you clicked');
-            console.log(event.target);
-            var colorToUse = $(event.target).css('background-color')
-        }
-        console.log("target color:", colorToUse);
-        console.log($('nav')[0]);
-        $('.text-color').css('color', `${colorToUse}`)
-
-    })
+    
 
 
 
